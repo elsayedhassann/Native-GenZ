63c501ecb15cf7ada2e6817a06d2865ca5001268
elsayedhassann <136090512+elsayedhassann@users.noreply.github.com>
2025-05-10 19:08:18 +0300

    Delete GenZ directory

--- GenZ/forget_pass.js
+++
@@ -1,5 +0,0 @@
-setTimeout(() => {
-    // Code to be executed after the delay (e.g., show success message)
-    successMessage.style.display = 'block';
-  }, 5000); // Delay of 5 seconds (5000 milliseconds)
-  
--- GenZ/logout_handle.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_unset();
-header("location:login_page.php");
-
-?>
